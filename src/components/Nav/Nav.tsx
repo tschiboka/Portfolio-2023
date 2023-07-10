7350d89717dd89f4e0dd67e94fa1a9174b4eb604
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2023-07-10 21:49:29 +0100

    Responsive Logo

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,11 +1,12 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 import { PiHamburgerDuotone } from "react-icons/pi";
+import icon from "../../assets/images/icon.svg";
 import "./Nav.scss";
 
 const Nav = () => {
     return (
         <header>
-            <PiHamburgerDuotone className="logo" />
+            <img className="t-logo" src={icon} alt="" />
             <RxHamburgerMenu className="burger" />
             <ul className="nav_links">
                 <li className="active">Home</li>
