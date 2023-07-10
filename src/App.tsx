81322ede24799e44632050dfb09ea46f872ed5e2
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2023-07-10 04:42:42 +0100

    Mobile Layout of Welcome Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import Welcome from "./components/Welcome/Welcome";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { PiHamburgerDuotone } from "react-icons/pi";
 import "./styles/palette.scss";
 import "./App.scss";
 
 function App() {
-    return <Welcome />;
+    return (
+        <>
+            <header>
+                <PiHamburgerDuotone className="logo" />
+                <RxHamburgerMenu className="logo" />
+            </header>
+            <Welcome />
+        </>
+    );
 }
 
 export default App;
--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,22 +1,27 @@
-import headshot from "../../assets/headshot_placeholder.jpg";
+import headshot from "../../assets/images/headshot_placeholder.png";
 import "./Welcome.scss";
 
 const Welcome = () => {
     return (
         <div className="Welcome">
             <img src={headshot} />
-            <p>
-                Hello there! I'm pleased that you've found your way here.
-                Whether you share my passion for coding and design or need a
-                supporting hand with web development, you've come to the right
-                place.
-            </p>
-            <p>
+            <h1>Tivadar Debnar | Web Developer</h1>
+            <div className="hero">
+                <p>
+                    <strong>Hello there!&nbsp;</strong>
+                    <br />
+                    <br />
+                    I'm pleased that you've found your way here. Whether you
+                    share my passion for coding and design or need a supporting
+                    hand with web development, you've come to the right place.
+                </p>
+            </div>
+            {/* <p>
                 I'm a dedicated web developer with a deep love for crafting
                 beautiful and functional websites and web applications. I strive
                 to create impressive digital experiences that leave an impact
                 and projects I'm proud of.
-            </p>
+            </p> */}
         </div>
     );
 };
