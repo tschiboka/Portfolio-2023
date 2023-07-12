d11fe15b361a5f7d8118d0ad8635170452524584
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2023-07-12 01:55:57 +0100

    Welcome Anomations

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Nav from "./components/Nav/Nav";
 import Welcome from "./components/Welcome/Welcome";
 import Menu from "./components/Menu/Menu";
 import SubNav from "./components/SubNav/SubNav";
+import Intro from "./components/Intro/Intro";
 import "./styles/palette.scss";
 import "./App.scss";
 
@@ -33,25 +34,7 @@ function App() {
                 <SubNav themeMode={themeMode} setThemeMode={setThemeMode} />
             )}
             <main>
-                <div className="hero">
-                    <p>
-                        <strong>Hello there!&nbsp;</strong>
-                        <br />
-                        <br />
-                        I'm pleased that you've found your way here. Whether you
-                        share my passion for coding and design or need a
-                        supporting hand with web development, you've come to the
-                        right place.
-                    </p>
-                </div>
-                {/* <p>
-                I'm a dedicated web developer with a deep love for crafting
-                beautiful and functional websites and web applications. I strive
-                to create impressive digital experiences that leave an impact
-                and projects I'm proud of.
-            </p> */}
-                <div className="rectangle"></div>
-                <div className="rectangle"></div>
+                <Intro />
             </main>
         </>
     );
---
+++ src/components/Intro/Intro.tsx
@@ -0,0 +1,25 @@
+import "./Intro.scss";
+
+const Intro = () => {
+    return (
+        <>
+            <h1>Hello there!&nbsp;</h1>
+            <br />
+            <p>
+                I'm pleased that you've found your way here. Whether you share
+                my passion for coding and design or need a supporting hand with
+                web development, you've come to the right place.
+            </p>
+            <br />
+            <p>
+                I'm a dedicated web developer with a deep love for crafting
+                beautiful and functional websites and web applications. I strive
+                to create impressive digital experiences that leave an impact
+                and projects I'm proud of.
+            </p>
+            <p></p>
+        </>
+    );
+};
+
+export default Intro;
