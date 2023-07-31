85fe92b01672fa379d8a09eb54137e93ae8de4ed
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2023-07-31 13:50:36 +0100

    Contact Form Input Validation

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -6,6 +6,7 @@ import { MdAlternateEmail } from "react-icons/md";
 import { FiPhone } from "react-icons/fi";
 import "./Contact.scss";
 import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
 
 interface Props {
     pageName: string;
@@ -17,6 +18,51 @@ interface Props {
     setSubMenuVisible: (visible: boolean) => void;
 }
 
+interface ValidationResult {
+    valid: boolean;
+    error: string;
+}
+
+const validateName = (name: string): ValidationResult => {
+    const nameRegex = /^[a-z \-']+$/i;
+    if (name.length === 0) return { valid: false, error: "Cannot be Empty!" };
+    if (name.length > 50) return { valid: false, error: "Too Long!" };
+    if (!nameRegex.test(name))
+        return { valid: false, error: "No Special Characters!" };
+    return { valid: true, error: "" };
+};
+
+const validateEmail = (email: string): ValidationResult => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (email.length === 0) return { valid: false, error: "Cannot be Empty!" };
+    if (!emailRegex.test(email))
+        return { valid: false, error: "Invalid Email Format!" };
+    return { valid: true, error: "" };
+};
+
+const validateTel = (tel: string): ValidationResult => {
+    if (tel.length > 0) {
+        // Let through empty tel nums
+        if (!/^[0-9 ]+$/.test(tel))
+            return { valid: false, error: "Only Numbers and Spaces Allowed!" };
+        if (tel.length < 10) return { valid: false, error: "Too Short!" };
+        if (tel.length > 16) return { valid: false, error: "Too Long!" };
+    }
+    return { valid: true, error: "" };
+};
+
+const validateMessage = (message: string): ValidationResult => {
+    const allowedCharactersRegex = /^[a-zA-Z0-9,.!?()\[\]:;@'"-\s]*$/;
+    if (message.length === 0)
+        return { valid: false, error: "Cannot be Empty!" };
+    if (message.length < 10)
+        return { valid: false, error: "Min 10 Characters!" };
+    if (message.length > 3000) return { valid: false, error: "Too Long!" };
+    if (!allowedCharactersRegex.test(message))
+        return { valid: false, error: "Illegal Characters in Text!" };
+    return { valid: true, error: "" };
+};
+
 const Contact = ({
     pageName,
     mobileMenuVisible,
@@ -26,8 +72,37 @@ const Contact = ({
     subMenuVisible,
     setSubMenuVisible,
 }: Props) => {
+    const nameRef = useRef<HTMLInputElement>(null);
+    const emailRef = useRef<HTMLInputElement>(null);
+    const telRef = useRef<HTMLInputElement>(null);
+    const messageRef = useRef<HTMLTextAreaElement>(null);
+
+    const [nameError, setNameError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [telError, setTelError] = useState("");
+    const [messageError, setMessageError] = useState("");
+
+    const handleSubmit = () => {
+        // References
+        let name = "";
+        let email = "";
+        let tel = "";
+        let message = "";
+
+        if (nameRef.current) name = nameRef.current.value;
+        if (emailRef.current) email = emailRef.current.value;
+        if (telRef.current) tel = telRef.current.value;
+        if (messageRef.current) message = messageRef.current.value;
+
+        // Validate
+        setNameError(validateName(name).error);
+        setEmailError(validateEmail(email).error);
+        setTelError(validateTel(tel).error);
+        setMessageError(validateMessage(message).error);
+    };
+
     const mail = "tibi.aki.tivadar@gmail.com";
-    const tel = "07474999334";
+    const tel = "+447474999334";
     return (
         <>
             <Nav
@@ -61,7 +136,7 @@ const Contact = ({
                     <li>
                         <FiPhone className="icon" />
                         <a href={`tel:${tel}`} className="link">
-                            07474999334
+                            {tel}
                         </a>
                     </li>
                 </ul>
@@ -71,25 +146,58 @@ const Contact = ({
                         <label htmlFor="name">
                             Name<span>*</span>
                         </label>
-                        <input id="name" type="text" />
+                        <input id="name" type="text" ref={nameRef} />
                     </fieldset>
+                    {nameError && (
+                        <span
+                            className="error-message"
+                            id="error-message--name"
+                        >
+                            {nameError}
+                        </span>
+                    )}
                     <fieldset>
                         <label htmlFor="name">
                             Email<span>*</span>
                         </label>
-                        <input id="email" type="text" />
+                        <input id="email" type="text" ref={emailRef} />
                     </fieldset>
+                    {emailError && (
+                        <span
+                            className="error-message"
+                            id="error-message--email"
+                        >
+                            {emailError}
+                        </span>
+                    )}
                     <fieldset>
                         <label htmlFor="name">Phone</label>
-                        <input id="phone" type="text" />
+                        <input id="phone" type="text" ref={telRef} />
                     </fieldset>
+                    {telError && (
+                        <span className="error-message" id="error-message--tel">
+                            {telError}
+                        </span>
+                    )}
                     <fieldset>
                         <label htmlFor="message">
                             Message<span>*</span>
                         </label>
-                        <textarea id="name" />
+                        <textarea id="name" ref={messageRef} />
                     </fieldset>
-                    <button className="submit-message" type="button">
+                    {messageError && (
+                        <span
+                            className="error-message"
+                            id="error-message--message"
+                        >
+                            {messageError}
+                        </span>
+                    )}
+                    <button
+                        className="submit-message"
+                        type="button"
+                        onClick={() => handleSubmit()}
+                    >
                         Send Your Message
                     </button>
                 </form>
