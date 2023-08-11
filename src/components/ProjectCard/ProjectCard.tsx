8b43018e2e4cc7f47058925da7e8665c835c640f
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2023-08-11 15:42:01 +0100

    Show Project Links on Image Click

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,48 +1,35 @@
+import { useState } from "react";
 import type { Project } from "../Projects/Projects";
+import { getColourName } from "../Projects/getProjects";
 import { Link } from "react-router-dom";
 import { FaGithub } from "react-icons/fa";
 import { TbWorldWww } from "react-icons/tb";
-import { FiMoreHorizontal } from "react-icons/fi";
+import { PiReadCvLogoFill } from "react-icons/pi";
 import "./ProjectCard.scss";
-import { getColourName } from "../Projects/getProjects";
 
 interface Props {
     project: Project;
 }
 
-const renderLinks = (url?: string, github?: string, readMoreLink?: string) => {
-    if (url || github || readMoreLink)
-        return (
-            <>
-                <hr />
-                <div className="ProjectCard__links">
-                    {url && (
-                        <Link to={url} title="View Project Website">
-                            <TbWorldWww />
-                        </Link>
-                    )}
-                    {github && (
-                        <Link to={github} title="Check Out Project Github">
-                            <FaGithub />
-                        </Link>
-                    )}
-                    {readMoreLink && (
-                        <Link to={readMoreLink} title="Read More About Project">
-                            <FiMoreHorizontal />
-                        </Link>
-                    )}
-                </div>
-            </>
-        );
-};
-
 const ProjectCard = ({
     project: { title, image, description, badges, url, github, readMoreLink },
 }: Props) => {
+    const [linkOptionsOpened, setLinkOptionsOpened] = useState(false);
     return (
         <div className="ProjectCard">
+            {linkOptionsOpened && (url || github || readMoreLink) && (
+                <div className="ProjectCard__link-options">
+                    {renderLinks(
+                        setLinkOptionsOpened,
+                        title,
+                        url,
+                        github,
+                        readMoreLink
+                    )}
+                </div>
+            )}
             <h3>{title}</h3>
-            <img src={image} />
+            <img src={image} onClick={() => setLinkOptionsOpened(true)} />
             <hr />
             <p>{description}</p>
             <hr />
@@ -56,9 +43,80 @@ const ProjectCard = ({
                     </span>
                 ))}
             </div>
-            {renderLinks(url, github, readMoreLink)}
+            {renderBottomLinks(url, github, readMoreLink)}
         </div>
     );
+};
+
+const renderBottomLinks = (
+    url?: string,
+    github?: string,
+    readMoreLink?: string
+) => {
+    if (url || github || readMoreLink)
+        return (
+            <>
+                <hr />
+                <div className="ProjectCard__bottom-links">
+                    {url && (
+                        <Link to={url} title="View Project Website">
+                            <TbWorldWww />
+                        </Link>
+                    )}
+                    {github && (
+                        <Link to={github} title="Check Out Project Github">
+                            <FaGithub />
+                        </Link>
+                    )}
+                    {readMoreLink && (
+                        <Link to={readMoreLink} title="Read More About Project">
+                            <PiReadCvLogoFill />
+                        </Link>
+                    )}
+                </div>
+            </>
+        );
+};
+
+const renderLinks = (
+    setLinkOptionsOpened: (opened: false) => void,
+    title: string,
+    url?: string,
+    github?: string,
+    readMoreLink?: string
+) => {
+    if (url || github || readMoreLink)
+        return (
+            <div
+                className="ProjectCard__links"
+                onClick={() => setLinkOptionsOpened(false)}
+            >
+                <h3>Links</h3>
+                <h3 className="ProjectCard__links__title">{title}</h3>
+                <hr />
+                <div className="link-box">
+                    {url && (
+                        <Link to={url} title="View Project Website">
+                            <TbWorldWww />
+                            <span>Website Link</span>
+                        </Link>
+                    )}
+                    {github && (
+                        <Link to={github} title="Check Out Project Github">
+                            <FaGithub />
+                            <span>Github Link</span>
+                        </Link>
+                    )}
+                    {readMoreLink && (
+                        <Link to={readMoreLink} title="Read More About Project">
+                            <PiReadCvLogoFill />
+                            <span>Blog Link</span>
+                        </Link>
+                    )}
+                </div>
+                <hr />
+            </div>
+        );
 };
 
 export default ProjectCard;
--- src/components/RiffMaster/RiffMaster.tsx
+++ src/components/RiffMaster/RiffMaster.tsx
@@ -921,7 +921,7 @@ const RiffMaster = ({
                     {codeSnippetMessaging}
                 </SyntaxHighlighter>
 
-                <BlogTimeStamp created="09.08.2023" />
+                <BlogTimeStamp created="06.08.2023" updated="09.08.2023" />
             </main>
             <Footer />
         </>
