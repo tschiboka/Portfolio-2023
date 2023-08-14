463b834b2e71cf3322066f80f60a0242e492e5e6
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2023-08-14 04:27:52 +0100

    Allow Localhost

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json({
 
 app.use(cors({
     methods: 'GET, POST, PUT, DELETE',
-    origin: ['https://tschiboka.co.uk', 'http://127.0.0.1:5000', 'http://localhost:5000', 'localhost:5000'] // Allowed Origins
+    origin: ['https://tschiboka.co.uk', 'http://127.0.0.1:5000', 'http://localhost:5000', 'localhost:5000', "localhost"] // Allowed Origins
 }));
 
 // Routes
