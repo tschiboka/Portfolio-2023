4876c56fca4ef25ae2630e57bbe888bec70860b8
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2023-08-15 04:28:54 +0100

    Complete Restructuring of Routes and Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import UnderConstruction from "./components/UnderConstruction/UnderConstruction";
 import Page from "./components/Page/Page";
 import Home from "./components/Home/Home";
@@ -9,7 +9,6 @@ import Contact from "./components/Contact/Contact";
 import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy";
 import RiffMaster from "./components/RiffMaster/RiffMaster";
 import "./styles/palette.scss";
-import "./App.scss";
 
 function App() {
     const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
@@ -28,118 +27,12 @@ function App() {
     return (
         <Routes>
             <Route
-                path="/"
-                element={
-                    <Page title="Tivadar Debnar | Home" path="/">
-                        <Home
-                            pageName="home"
-                            mobileMenuVisible={mobileMenuVisible}
-                            setMobileMenuVisible={setMobileMenuVisible}
-                            themeMode={themeMode}
-                            setThemeMode={setThemeMode}
-                            subMenuVisible={subMenuVisible}
-                            setSubMenuVisible={setSubMenuVisible}
-                            path="/"
-                        />
-                    </Page>
-                }
-            />
-
-            <Route
-                path="/about"
-                element={
-                    <Page title="Tivadar Debnar | About" path="/about">
-                        <About
-                            pageName="about"
-                            mobileMenuVisible={mobileMenuVisible}
-                            setMobileMenuVisible={setMobileMenuVisible}
-                            themeMode={themeMode}
-                            setThemeMode={setThemeMode}
-                            subMenuVisible={subMenuVisible}
-                            setSubMenuVisible={setSubMenuVisible}
-                            path="/about"
-                        />
-                    </Page>
-                }
-            />
-
-            <Route
-                path="/projects"
-                element={
-                    <Page title="Tivadar Debnar | Projects" path="/projects">
-                        <Projects
-                            pageName="projects"
-                            mobileMenuVisible={mobileMenuVisible}
-                            setMobileMenuVisible={setMobileMenuVisible}
-                            themeMode={themeMode}
-                            setThemeMode={setThemeMode}
-                            subMenuVisible={subMenuVisible}
-                            setSubMenuVisible={setSubMenuVisible}
-                            path="/projects"
-                        />
-                    </Page>
-                }
-            />
-
-            <Route
-                path="/contact"
-                element={
-                    <Page title="Tivadar Debnar | Contact" path="/contact">
-                        <Contact
-                            pageName="contact"
-                            mobileMenuVisible={mobileMenuVisible}
-                            setMobileMenuVisible={setMobileMenuVisible}
-                            themeMode={themeMode}
-                            setThemeMode={setThemeMode}
-                            subMenuVisible={subMenuVisible}
-                            setSubMenuVisible={setSubMenuVisible}
-                            path="/contact"
-                        />
-                    </Page>
-                }
-            />
-
-            {/* Nested Subpages */}
-
-            <Route
-                path="/privacy-policy"
-                element={
-                    <Page
-                        title="Tivadar Debnar | Privacy Policy"
-                        path="/privacy-policy"
-                    >
-                        <PrivacyPolicy
-                            pageName="privacy-policy"
-                            mobileMenuVisible={mobileMenuVisible}
-                            setMobileMenuVisible={setMobileMenuVisible}
-                            themeMode={themeMode}
-                            setThemeMode={setThemeMode}
-                            subMenuVisible={subMenuVisible}
-                            setSubMenuVisible={setSubMenuVisible}
-                            path="/privacy-policy"
-                        />
-                    </Page>
-                }
-            />
-
-            <Route
                 path="/blog/riffmaster"
                 element={
                     <Page
                         title="Tivadar Debnar | RiffMaster"
                         path="/blog/riffmaster"
-                    >
-                        <RiffMaster
-                            pageName="riffmaster"
-                            mobileMenuVisible={mobileMenuVisible}
-                            setMobileMenuVisible={setMobileMenuVisible}
-                            themeMode={themeMode}
-                            setThemeMode={setThemeMode}
-                            subMenuVisible={subMenuVisible}
-                            setSubMenuVisible={setSubMenuVisible}
-                            path="/blog/riffmaster"
-                        />
-                    </Page>
+                    ></Page>
                 }
             />
         </Routes>
