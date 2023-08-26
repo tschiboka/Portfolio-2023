97f588615e9f262e6ae8c3bbcf4d04825a38f36f
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2023-08-26 04:41:33 +0100

    Incognito Mode Does Not Record Visits

--- src/components/sharedComponents/Page/Page.tsx
+++ src/components/sharedComponents/Page/Page.tsx
@@ -1,7 +1,8 @@
 import { ReactNode, useEffect } from "react";
 import { postVisit } from "../../../serverAPI/visits";
-import "./Page.scss";
+import { detectIncognito } from "detectincognitojs";
 import Overlay from "../Overlay/Overlay";
+import "./Page.scss";
 
 interface Props {
     children: ReactNode;
@@ -13,7 +14,11 @@ const Page = ({ children, title, path }: Props) => {
     useEffect(() => {
         document.title = title;
         window.scrollTo(0, 0);
-        postVisit(path);
+
+        // Do Not Record Visits in Incognito Mode
+        detectIncognito().then((result) => {
+            if (!result.isPrivate) postVisit(path);
+        });
     }, []);
 
     return (
