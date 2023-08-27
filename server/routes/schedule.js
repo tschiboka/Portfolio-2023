ba6882c888f811ea114a746bbc71bdab17b01cec
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2023-08-27 16:30:33 +0100

    Await Email Sent

--- server/routes/schedule.js
+++ server/routes/schedule.js
@@ -2,8 +2,8 @@ const express = require("express");
 const dailyEmail = require("../scheduled/dailyEmail");
 const route = express.Router();
 
-route.post("/daily-breakdown", (req, res) => {
-    dailyEmail();
+route.post("/daily-breakdown", async (req, res) => {
+    await dailyEmail();
     res.status(200).json({ success: true });
 });
 
