57a6cdbc701bce1cf4b0a36faf53ff01e5c1c9fa
Tivadar Debnar <tibi.aki.tivadar@gmail.com>
2023-12-17 05:05:56 +0000

    Fix scrollbar, figure padding and progress bars

--- src/components/sharedComponents/Page/Page.tsx
+++ src/components/sharedComponents/Page/Page.tsx
@@ -1,32 +1,73 @@
-import { ReactNode, useEffect } from "react";
-import { postVisit } from "../../../serverAPI/visits";
-import { detectIncognito } from "detectincognitojs";
-import Overlay from "../Overlay/Overlay";
-import "./Page.scss";
+import { ReactNode, useEffect, useMemo, useRef } from 'react'
+import { useAppContext } from '../../../context/AppContext'
+import { postVisit } from '../../../serverAPI/visits'
+import { detectIncognito } from 'detectincognitojs'
+import Overlay from '../Overlay/Overlay'
+import './Page.scss'
 
 interface Props {
-    children: ReactNode;
-    title: string;
-    path: string;
+    children: ReactNode
+    title: string
+    path: string
 }
 
 const Page = ({ children, title, path }: Props) => {
+    const {
+        isPageScrolling,
+        setIsPageScrolling,
+        scrollTimeElapsed,
+        setScrollTimeElapsed,
+    } = useAppContext()
+    const isPageScrollingRef = useRef(isPageScrolling)
+    const setIsPageScrollingRef = useRef(setIsPageScrolling)
+    const scrollTimeElapsedRef = useRef(scrollTimeElapsed)
+    const setScrollTimeElapsedRef = useRef(setScrollTimeElapsed)
+
     useEffect(() => {
-        document.title = title;
-        window.scrollTo(0, 0);
+        isPageScrollingRef.current = isPageScrolling
+        scrollTimeElapsedRef.current = scrollTimeElapsed
+    }, [isPageScrolling, scrollTimeElapsed])
+
+    useEffect(() => {
+        document.title = title
 
         detectIncognito().then((result) => {
             // Do Not Record Visits in Incognito Mode
-            if (!result.isPrivate) postVisit(path);
-        });
-    }, []);
+            if (!result.isPrivate) postVisit(path)
+        })
+
+        window.removeEventListener('scroll', handleScroll)
+        window.scrollTo(0, 0)
+        window.addEventListener('scroll', handleScroll, { passive: true })
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
+
+    const scrollTimeout = setInterval(() => {
+        const timeElapsed = Date.now() - scrollTimeElapsedRef.current
+        if (isPageScrollingRef.current && timeElapsed > 1000) {
+            setIsPageScrollingRef.current(false)
+            setScrollTimeElapsedRef.current(Date.now())
+            document.body.classList.remove('scroll')
+            clearTimeout(scrollTimeout)
+        }
+    }, 100)
+
+    const handleScroll = () => {
+        const timeElapsed = Date.now() - scrollTimeElapsedRef.current
+        setScrollTimeElapsedRef.current(Date.now())
+        if (!isPageScrollingRef.current) {
+            setIsPageScrollingRef.current(true)
+            document.body.classList.add('scroll')
+            clearTimeout(timeElapsed)
+        }
+    }
 
     return (
-        <>
+        <div className="Page">
             {children}
             <Overlay></Overlay>
-        </>
-    );
-};
+        </div>
+    )
+}
 
-export default Page;
+export default Page
